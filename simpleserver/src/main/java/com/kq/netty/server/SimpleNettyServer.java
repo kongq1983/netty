cca6ba21f31deee19903d56921eca195e30405c1
king <king@qq.com>
2019-06-29 16:06:14 +0800

    格式化

--- simpleserver/src/main/java/com/kq/netty/server/SimpleNettyServer.java
+++ simpleserver/src/main/java/com/kq/netty/server/SimpleNettyServer.java
@@ -12,6 +12,7 @@ import io.netty.util.CharsetUtil;
 
 /**
  * SimpleNettyServer
+ *
  * @author kq
  * @date 2019-01-08
  */
@@ -19,43 +20,44 @@ public class SimpleNettyServer {
 
     private static final String IP = "127.0.0.1";
     public static final int PORT = 15656;
-    private static final int BIZGROUPSIZE = Runtime.getRuntime().availableProcessors()*2;
+    private static final int BIZGROUPSIZE = Runtime.getRuntime().availableProcessors() * 2;
 
     private static final int BIZTHREADSIZE = 100;
     private static final EventLoopGroup bossGroup = new NioEventLoopGroup(BIZGROUPSIZE);
     private static final EventLoopGroup workerGroup = new NioEventLoopGroup(BIZTHREADSIZE);
+
     public static void service() throws Exception {
-                ServerBootstrap bootstrap = new ServerBootstrap();
-               bootstrap.group(bossGroup, workerGroup);
-               bootstrap.channel(NioServerSocketChannel.class);
-                bootstrap.childHandler(new ChannelInitializer<Channel>() {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup);
+        bootstrap.channel(NioServerSocketChannel.class);
+        bootstrap.childHandler(new ChannelInitializer<Channel>() {
 
-                    @Override
+            @Override
             protected void initChannel(Channel ch) throws Exception {
-                                // TODO Auto-generated method stub
-                                ChannelPipeline pipeline = ch.pipeline();
-                                 pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
-                                    pipeline.addLast(new LengthFieldPrepender(4));
-                                   pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
-                                    pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
-                                    pipeline.addLast(new TcpServerHandler());
-                            }
+                // TODO Auto-generated method stub
+                ChannelPipeline pipeline = ch.pipeline();
+                pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+                pipeline.addLast(new LengthFieldPrepender(4));
+                pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+                pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+                pipeline.addLast(new TcpServerHandler());
+            }
 
-                });
-                  ChannelFuture f = bootstrap.bind(IP, PORT).sync();
-                    f.channel().closeFuture().sync();
-                    System.out.println("TCP服务器已启动  监听端口:"+PORT);
-               }
+        });
+        ChannelFuture f = bootstrap.bind(IP, PORT).sync();
+        f.channel().closeFuture().sync();
+        System.out.println("TCP服务器已启动  监听端口:" + PORT);
+    }
 
-                protected static void shutdown() {
-                    workerGroup.shutdownGracefully();
-                    bossGroup.shutdownGracefully();
-               }
+    protected static void shutdown() {
+        workerGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
+    }
 
-                public static void main(String[] args) throws Exception {
-                    System.out.println("开始启动TCP服务器...");
-                    SimpleNettyServer.service();
+    public static void main(String[] args) throws Exception {
+        System.out.println("开始启动TCP服务器...");
+        SimpleNettyServer.service();
         //            SimpleNettyServer.shutdown();
-                }
+    }
 
 }
